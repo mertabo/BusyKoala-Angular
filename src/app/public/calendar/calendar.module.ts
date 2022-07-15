133ca511ae028c64e514aa5246408f67186b38e5
mrtabo1 <mrtabo1@up.edu.ph>
2022-07-15 19:06:08 +0800

    feat: add create event form for the calendar

--- src/app/public/calendar/calendar.module.ts
+++ src/app/public/calendar/calendar.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzTimePickerModule } from 'ng-zorro-antd/time-picker';
 
 import { CalendarRoutingModule } from './calendar-routing.module';
 
@@ -9,7 +15,15 @@ import { MonthsComponent } from './months/months.component';
 
 @NgModule({
   declarations: [CalendarComponent, CreateEventFormComponent, MonthsComponent],
-  imports: [CommonModule, CalendarRoutingModule],
+  imports: [
+    CommonModule,
+    CalendarRoutingModule,
+    NzFormModule,
+    NzInputModule,
+    NzDatePickerModule,
+    NzTimePickerModule,
+    ReactiveFormsModule,
+  ],
   exports: [MonthsComponent],
 })
 export class CalendarModule {}
--- src/app/public/calendar/create-event-form/create-event-form.component.ts
+++ src/app/public/calendar/create-event-form/create-event-form.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-event-form',
   templateUrl: './create-event-form.component.html',
-  styleUrls: ['./create-event-form.component.css']
+  styleUrls: ['./create-event-form.component.css'],
 })
 export class CreateEventFormComponent implements OnInit {
+  createEventForm = this.fb.group({
+    title: [null, Validators.required],
+    date: [null, Validators.required],
+    time: [null],
+    workplace: [null],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  markAsInvalid(formControl: AbstractControl): void {
+    formControl.markAsDirty();
+    formControl.updateValueAndValidity({ onlySelf: true });
   }
 
+  handleFocusOut(formControlName: string): void {
+    const formControl = this.createEventForm.controls[formControlName];
+    if (formControl.invalid) this.markAsInvalid(formControl);
+  }
+
+  submitForm(): void {
+    const titleControl = this.createEventForm.controls['title'];
+    const dateControl = this.createEventForm.controls['date'];
+    const timeControl = this.createEventForm.controls['time'];
+    const workplaceControl = this.createEventForm.controls['workplace'];
+
+    if (this.createEventForm.valid) {
+      // Event name format: Title (Time @ Workplace)
+      let newEvent = titleControl.value;
+
+      if (timeControl.value || workplaceControl.value) {
+        newEvent = `${newEvent} (`;
+
+        if (timeControl.value) {
+          newEvent = `${newEvent}${timeControl.value}`;
+
+          if (workplaceControl.value) {
+            newEvent = `${newEvent} @ `;
+          }
+        }
+
+        if (workplaceControl.value) {
+          newEvent = `${newEvent}${workplaceControl.value}`;
+        }
+
+        newEvent = `${newEvent})`;
+      }
+
+      console.log(newEvent);
+    } else {
+      if (titleControl.invalid) this.markAsInvalid(titleControl);
+      if (dateControl.invalid) this.markAsInvalid(dateControl);
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
 }
