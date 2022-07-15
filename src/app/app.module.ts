7d040b4493000aec506c6e7c51c6343ca0a31900
mrtabo1 <mrtabo1@up.edu.ph>
2022-07-15 15:04:36 +0800

    build: downgrade to angular13 and install ng-zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -8,16 +16,21 @@ import { AppComponent } from './app.component';
 import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
 
+registerLocaleData(en);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
     AuthModule,
     AppRoutingModule,
     PublicModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
