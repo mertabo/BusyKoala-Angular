fe0ce2892ddb3841a86b56a554d7225b9cf7aaae
mrtabo1 <mrtabo1@up.edu.ph>
2022-07-20 15:46:06 +0800

    feat: update WorkspacesListComponent UI everytime user selects another workspace

--- src/app/main/workspaces/workspaces-list/workspaces-list.component.ts
+++ src/app/main/workspaces/workspaces-list/workspaces-list.component.ts
@@ -1,41 +1,72 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WorkspacesService } from '../workspaces.service';
 import { Workspace } from '../workspaces';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  Event,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router,
+} from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-workspaces-list',
   templateUrl: './workspaces-list.component.html',
   styleUrls: ['./workspaces-list.component.css'],
 })
-export class WorkspacesListComponent implements OnInit {
+export class WorkspacesListComponent implements OnInit, OnDestroy {
   workspaces: Workspace[] = [];
-  selectedWorkspace?: Workspace;
+  selectedWorkspaceID?: string;
+  routeSubscription!: Subscription;
 
   getWorkspaces(): void {
     this.workspacesService.getWorkspaces().subscribe((data: any) => {
       this.workspaces = data;
 
       if (data) {
-        this.selectedWorkspace = data[0];
-        this.router.navigate([this.selectedWorkspace!.id], {
+        this.selectedWorkspaceID = data[0].id;
+        this.router.navigate([this.selectedWorkspaceID], {
           relativeTo: this.route,
         });
       }
     });
   }
 
-  selectWorkspace(workspace: Workspace): void {
-    this.selectedWorkspace = workspace;
+  selectWorkspace(workspaceID: string): void {
+    this.selectedWorkspaceID = workspaceID;
   }
 
   constructor(
     private workspacesService: WorkspacesService,
     private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) {
+    // https://www.angularjswiki.com/angular/how-to-detect-route-change-in-angular-with-examples/
+    this.routeSubscription = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationStart) {
+        // Show progress spinner or progress bar
+        // console.log('Route change detected');
+      }
+      if (event instanceof NavigationEnd) {
+        // Hide progress spinner or progress bar
+        this.selectedWorkspaceID = event.url.split('/')[2];
+      }
+      if (event instanceof NavigationError) {
+        // Hide progress spinner or progress bar
+        // Present error to user
+        console.log(event.error);
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.getWorkspaces();
+  }
+
+  // https://www.codingcatlady.net/2017/12/Unsubscribe-from-Navigation-Events.html
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
   }
 }
